b8567f2e63d9fb51e9649564a0845804cfb311be
siriwat.c <siriwat.chin@gmail.com>
2019-08-13 14:55:10 +0700

    Change internal lib at errors.go

--- errors.go
+++ errors.go
@@ -9,7 +9,7 @@ import (
 	"fmt"
 	"reflect"
 
-	"github.com/teera123/gin/internal/json"
+	"github.com/joijiprogram/gin/internal/json"
 )
 
 // ErrorType is an unsigned 64-bit error code as defined in the gin spec.
